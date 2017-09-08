b0104e006bbfea71a75a60eb37f9dc9f73a8fbb8
Joseph Abell <joseph.abell@piksel.com>
2017-09-08 07:44:39 +0100

    Add basic homepage

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <main>
+    <h1>{{ homepage.Title }}</h1>
+    <h2 v-if='homepage.Subtitle'>{{ homepage.Subtitle }}</h2>
+  </main>
+</template>
+
+<script>
+import { db } from '@/firebase.js'
+
+export default {
+  name: 'Missing',
+  firebase: function () {
+    console.log(db)
+    return {
+      homepage: {
+        source: db.ref('data/homepage'),
+        asObject: true
+      }
+    }
+  }
+}
+</script>
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
+import Home from '@/components/Home'
 import Missing from '@/components/Missing'
 
 Vue.use(Router)
@@ -7,6 +9,11 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
     {
       path: '*',
       name: 'Missing',

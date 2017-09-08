fa340bc3a2c34e51a5da98ea8b20a837f62c4d80
Joseph Abell <joseph.abell@piksel.com>
2017-09-08 09:02:23 +0100

    Add loading spinner when firebase hasn't finished loading

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,25 +1,38 @@
 <template>
   <main>
-    <h1>{{ homepage.Title }}</h1>
-    <h2 v-if='homepage.Subtitle'>{{ homepage.Subtitle }}</h2>
+    <div v-if='loaded'>
+      <h1>{{ homepage.Title }}</h1>
+      <h2 v-if='homepage.Subtitle'>{{ homepage.Subtitle }}</h2>
+    </div>
+    <Loading v-else />
   </main>
 </template>
 
 <script>
 import { db } from '@/firebase.js'
+import Loading from '@/components/Loading'
 
 export default {
-  name: 'Missing',
+  name: 'Homepage',
+  data: function () {
+    return {
+      loaded: false
+    }
+  },
+  components: {
+    Loading
+  },
   firebase: function () {
-    console.log(db)
     return {
       homepage: {
         source: db.ref('data/homepage'),
-        asObject: true
+        asObject: true,
+        readyCallback: () => {
+          this.loaded = true
+        }
       }
     }
   }
 }
 </script>
-
 
---
+++ src/components/Loading.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class='loading'>
+    <div class='spinner'>
+      <Loader color='#90c5c2' />
+    </div>
+    <h1>Loading...</h1>
+  </div>
+</template>
+
+<script>
+import Loader from 'vue-spinner/src/MoonLoader'
+
+export default {
+  name: 'Loading',
+  components: {
+    Loader
+  }
+}
+</script>
+
+<style>
+  .loading {
+    color: #90c5c2;
+    text-align: center;
+  }
+
+  .loading h1 {
+    color: #90c5c2;
+  }
+
+  .spinner {
+    width: 77px;
+    margin: 0 auto;
+  }
+
+  .v-moon1 {
+    width: 77px !important;
+    height: 77px !important;
+  }
+</style>

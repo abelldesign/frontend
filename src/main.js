2daaa530635b44345311b6db9f8891c433628a8a
Joseph Abell <joseph.abell@piksel.com>
2017-11-14 21:08:19 +0000

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import firebase from 'firebase'
 import App from './App'
 import router from './router'
 import { store } from './store'
 import { firebaseConfig } from './config'
+
+Vue.use(VueAnalytics, {
+  id: 'UA-102804990-1'
+})
 
 Vue.config.productionTip = false
 firebase.initializeApp(firebaseConfig)
